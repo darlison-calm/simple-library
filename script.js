dfdb3112dc91c93cf2514e0f9918b9f0b5dd5e3f
darlison-calm <bernardo.darlison@gmail.com>
2024-05-15 15:11:36 -0300

    Add form submission handling and book display
    - Implement form submission event listener to prevent default behavior, gather input data, add new book to library, display the book, reset the form, and close the form popup.
    - Create getBookFromInput function to extract book details from form inputs and return a new Book instance.
    - Update displayLibrary function to clear the books container and display all books from the library.
    - Ensure that the form is reset after each submission to clear input fields.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myLibrary = [];
+const myLibrary = []
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -7,37 +7,89 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary() {
-  const newBook = new Book(title, author, pages, read);
-  myLibrary.push(newBook);
+function addBookToLibrary(book) {
+  myLibrary.push(book);
 }
 
 // User interface
-const form = document.getElementById("form-popup");
+const formPopup = document.getElementById("form-popup");
 const overlay = document.getElementById("overlay");
 const addBookBtn = document.getElementById("addBookBtn");
+const booksContainer = document.getElementById("booksGrid");
+const userInputBook = document.querySelector(".add-book-form");
 
-function openForm() {
+function openForm(){
   overlay.style.display = "block";
-  form.style.display = "block";
+  formPopup.style.display = "block";
 }
-
-function closeForm() {
+function closeForm(){
   overlay.style.display = "none";
-  form.style.display = "none";
+  formPopup.style.display = "none";
 }
 
 // Open form after clicking the add book button
 addBookBtn.addEventListener("click" , openForm);
 
 // Close form after clicking outside the form
-window.addEventListener('mouseup' , function(e){
-  if(!form.contains(e.target) && e.target !== addBookBtn){
-      closeForm();
-  }
+window.addEventListener('mouseup' , (e) => !formPopup.contains(e.target) && closeForm());
+
+// Display book on the page
+function displayBook(book) {
+  const bookCard = document.createElement("div");
+  const title = document.createElement("p");
+  const author = document.createElement("p");
+  const pages = document.createElement("p");
+  const readBtn = document.createElement("button");
+  const btnGroup = document.createElement("div");
+  const removeBtn = document.createElement("button");
+
+  title.textContent = `"${book.title}"`
+  author.textContent = book['author'];
+  pages.textContent = `${book.pages} pages`;
+  removeBtn.textContent = 'Remove';
+
+  readBtn.textContent = book.read ? 'Read ✔' : 'Not read';
+
+  bookCard.classList.add("book-card");
+  btnGroup.classList.add("button-group");
+  removeBtn.classList.add("btn");
+  readBtn.classList.add("btn");
+
+  bookCard.appendChild(title);
+  bookCard.appendChild(author);
+  bookCard.appendChild(pages);
+  bookCard.appendChild(btnGroup);
+  
+  btnGroup.appendChild(readBtn);
+  btnGroup.appendChild(removeBtn); 
+  
+  booksContainer.appendChild(bookCard);
+}
+
+
+userInputBook.addEventListener("submit", (e) =>{
+  e.preventDefault();
+  const book = getBookFromInput()
+  addBookToLibrary(book)
+  displayBook(book)
+  userInputBook.reset();
+  closeForm()
 })
 
+function getBookFromInput() {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const read = document.getElementById('read-status').checked;
+ 
+  return new Book(title, author, pages, read)
+}
 
+function displayLibrary(){
+  booksContainer.innerHTML = '';
+  myLibrary.forEach((book) => displayBook(book));
+};
 
+displayLibrary()
 
 
